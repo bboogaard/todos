8e5497585d748791740a15035c5b781fb7bd94ea
Eagle <eagle@Eagles-Mac-mini.local>
2021-06-19 21:46:34 +0200

    Add docker-files

---
+++ app/settings-docker.py
@@ -0,0 +1,13 @@
+from app.settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todos',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
