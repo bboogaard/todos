5e66998084036affe8bdbc207173e798fb6a4d05
bboogaard <padawan@hetnet.nl>
2021-12-22 23:09:48 +0100

    Fix test setting

--- app/settings-test.py
+++ app/settings-test.py
@@ -3,3 +3,11 @@ from app.settings import *
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/test')
 PRIVATE_STORAGE_ROOT = os.path.join(BASE_DIR,'media/test/private')
+
+# Search
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index/test'),
+    },
+}
