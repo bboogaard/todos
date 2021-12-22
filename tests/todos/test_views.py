535268b296962c98420fd3b700107fb8010f046d
bboogaard <padawan@hetnet.nl>
2021-12-22 23:06:23 +0100

    Add tests for the search view

--- tests/todos/test_views.py
+++ tests/todos/test_views.py
@@ -44,6 +44,34 @@ class TestIndexView(TodosViewTest):
         self.assertEqual(response.status_code, 200)
 
 
+class TestSearchView(TodosViewTest):
+
+    def setUp(self):
+        super().setUp()
+        self.todos = [
+            TodoFactory(description='Foo'),
+            TodoFactory(description='Bar', status=Note.INACTIVE_STATUS)
+        ]
+        self.notes = [
+            NoteFactory(text='Foo'),
+            NoteFactory(text='Bar', status=Note.INACTIVE_STATUS),
+        ]
+        self.events = [
+            EventFactory(description='Foo')
+        ]
+        call_command('rebuild_index', interactive=False)
+
+    def test_get(self):
+        response = self.app.get('/search/?q=Foo', user=self.test_user)
+        self.assertEqual(response.status_code, 200)
+        response.mustcontain('Foo - Todo', 'Foo - Note', 'Foo - Event')
+
+    def test_get_inactive(self):
+        response = self.app.get('/search/?q=Bar', user=self.test_user)
+        self.assertEqual(response.status_code, 200)
+        response.mustcontain('Bar - Todo', no=['Bar - Note'])
+
+
 class TestTodosSaveJson(TodosViewTest):
 
     csrf_checks = False
